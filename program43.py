e3ef35a38a34ed1d059caa41699659d156270331
NameIsAlex <alexseveemail@gmail.com>
2016-11-06 14:21:57 -0500

    Create program43.py

---
+++ program43.py
@@ -0,0 +1,11 @@
+def main():
+    start = int(input("How many lines?"))
+
+    for e in range(start, 0, -1):
+        for d in range(start - e):
+            print(' ', end='')
+        for d in range(e):
+            print('*', end='')
+        print()
+
+main()
